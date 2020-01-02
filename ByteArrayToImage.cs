76cc45af1deedd81d50d79108b4fffac874e8a08
HYEJIN HWANG <snug1007@gmail.com>
2020-01-02 13:07:26 +0900

    Add files via upload

---
+++ ByteArrayToImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByteArrayToImage : MonoBehaviour {
+
+	public GameObject canvasImage;
+
+	void Start () {
+		
+	}
+	
+	private void Set_Image(byte[] recevByteArr)
+	{
+		try
+		{
+			Texture2D bmp;
+			bmp = new Texture2D(8, 8);
+			bmp.LoadImage(recevByteArr, false);
+
+			Vector2 pivot = new Vector2(0.5f, 0.5f);
+			Rect tRect = new Rect(0, 0, bmp.width, bmp.height);
+			Sprite newSprite = Sprite.Create(bmp, tRect, pivot);
+
+			GameObject webcam = GameObject.Find("Webcam");
+			canvasImage.GetComponent<Image>().overrideSprite = newSprite;
+
+		}
+		catch
+		{
+		}
+	}
+}
